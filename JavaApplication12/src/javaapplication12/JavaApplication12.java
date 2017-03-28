ba099f55acdedb29e5779ef5d9ca77f9a0960d77
heathen <heathen@sixth>
2017-03-28 20:13:28 +0300

    2

--- JavaApplication12/src/javaapplication12/JavaApplication12.java
+++ JavaApplication12/src/javaapplication12/JavaApplication12.java
@@ -15,7 +15,7 @@ public class JavaApplication12 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // first
+        // second
     }
     
 }
